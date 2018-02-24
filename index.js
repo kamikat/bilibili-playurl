fa2403f6f86e3276196690331afcc1540474908b
kamikat <kamikat@banana.moe>
2018-02-25 02:22:58 +0800

    fix optional argument handling

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var c = require('./vm-159')
   , I = c.cwrap("r", null, "string number number number string string number".split(" "))
   , g = c.cwrap("s", null, ["number"]);
 module.exports = function (cid, options) {
-  var domains = ["interface.bilibili.com/v2/playurl?", "bangumi.bilibili.com/player/web_api/v2/playurl?", "bangumi.bilibili.com/player/web_api/playurl?"];
-  var quality = 0, season_type;
   if ('number' == typeof options) {
-    quality = parseInt(options);
-    options = {};
+    options = {
+      quality: parseInt(options)
+    };
+  } else {
+    options = options || {};
   }
-  if (options) {
-    quality = options.quality || quality;
-    delete options.quality;
-    season_type = options.season_type || 0;
-    delete options.season_type;
-  }
+  var quality = options.quality || 0
+    , season_type = options.season_type || 0
+    , domains = [ "interface.bilibili.com/v2/playurl?", "bangumi.bilibili.com/player/web_api/v2/playurl?", "bangumi.bilibili.com/player/web_api/playurl?" ];
+  delete options.season_type;
+  delete options.quality;
   return new Promise(function (resolve, reject) {
     g(c.XB.addFunction(function (b, e) {
       b = c.TB(b);
